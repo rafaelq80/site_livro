a63bd2e76e3e0b8968113af9c7c12d11dfc89e6a
Rafael Queiroz <rafaelproinfo@gmail.com>
2026-05-25 22:17:32 -0300

    🐛 FIX:  Variáveis de Ambiente

--- api/send-email.js
+++ api/send-email.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
     body: JSON.stringify({
       service_id:  process.env.EMAILJS_SERVICE_ID,
       template_id: process.env.EMAILJS_TEMPLATE_ID,
-      user_id:     process.env.EMAILJS_PUBLIC_KEY,
+      user_id:     process.env.EMAILJS_PRIVATE_KEY,
       template_params: { name, email, whatsapp, message }
     })
   })
